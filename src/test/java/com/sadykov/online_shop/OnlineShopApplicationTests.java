a642cff213cd629b6236ca17a6e8c0ef4863b0fd
Sadykov Ramis <ramis.sadykov.99@mail.ru>
2025-12-12 20:14:44 +0300

    Init commit

---
+++ src/test/java/com/sadykov/online_shop/OnlineShopApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sadykov.online_shop;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OnlineShopApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
