76c6f5a9328ad0c38db17663e1b07dc172332f24
Sadykov Ramis <ramis.sadykov.99@mail.ru>
2025-12-14 23:09:07 +0300

    add интерфейс доменного репозитория

---
+++ src/main/java/com/sadykov/online_shop/domain/product/ProductRepository.java
@@ -0,0 +1,15 @@
+package com.sadykov.online_shop.domain.product;
+
+import java.util.List;
+import java.util.Optional;
+
+public interface ProductRepository {
+
+    Product save(Product product);
+
+    Optional<Product> findById(Long id);
+
+    List<Product> findAll();
+
+    void deleteById(Long id);
+}
