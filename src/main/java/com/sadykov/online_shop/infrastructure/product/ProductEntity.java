4e24338391456ca4ed1aa307d955e6ebae2dc23b
Sadykov Ramis <ramis.sadykov.99@mail.ru>
2025-12-15 18:22:42 +0300

    add Jpa-entity

---
+++ src/main/java/com/sadykov/online_shop/infrastructure/product/ProductEntity.java
@@ -0,0 +1,112 @@
+package com.sadykov.online_shop.infrastructure.product;
+
+import jakarta.persistence.*;
+
+import java.math.BigDecimal;
+import java.time.LocalDateTime;
+
+@Entity
+@Table(name = "products")
+public class ProductEntity {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String name;
+    private String description;
+
+    private BigDecimal price;
+
+    private int quantity;
+
+    private String category;
+
+    private String status;
+
+    private LocalDateTime createdAt;
+
+    protected ProductEntity(){}
+
+    public ProductEntity(Long id,
+                         String name,
+                         String description,
+                         BigDecimal price,
+                         int quantity,
+                         String category,
+                         String status,
+                         LocalDateTime createdAt) {
+        this.id = id;
+        this.name = name;
+        this.description = description;
+        this.price = price;
+        this.quantity = quantity;
+        this.category = category;
+        this.status = status;
+        this.createdAt = createdAt;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public BigDecimal getPrice() {
+        return price;
+    }
+
+    public void setPrice(BigDecimal price) {
+        this.price = price;
+    }
+
+    public int getQuantity() {
+        return quantity;
+    }
+
+    public void setQuantity(int quantity) {
+        this.quantity = quantity;
+    }
+
+    public String getCategory() {
+        return category;
+    }
+
+    public void setCategory(String category) {
+        this.category = category;
+    }
+
+    public String getStatus() {
+        return status;
+    }
+
+    public void setStatus(String status) {
+        this.status = status;
+    }
+
+    public LocalDateTime getCreatedAt() {
+        return createdAt;
+    }
+
+    public void setCreatedAt(LocalDateTime createdAt) {
+        this.createdAt = createdAt;
+    }
+}
