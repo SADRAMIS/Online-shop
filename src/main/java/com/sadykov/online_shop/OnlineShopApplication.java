25b02aa02a71dc9a8137e2e910c122d545df80e1
Sadykov Ramis <ramis.sadykov.99@mail.ru>
2025-12-23 18:39:50 +0300

    add инициализация тестовых данных

--- src/main/java/com/sadykov/online_shop/OnlineShopApplication.java
+++ src/main/java/com/sadykov/online_shop/OnlineShopApplication.java
@@ -1,7 +1,14 @@
 package com.sadykov.online_shop;
 
+import com.sadykov.online_shop.domain.product.Product;
+import com.sadykov.online_shop.domain.product.ProductRepository;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+
+import java.math.BigDecimal;
+import java.time.LocalDateTime;
 
 @SpringBootApplication
 public class OnlineShopApplication {
@@ -10,4 +17,22 @@ public class OnlineShopApplication {
 		SpringApplication.run(OnlineShopApplication.class, args);
 	}
 
+	@Bean
+	CommandLineRunner initProducts(ProductRepository productRepository) {
+		return args -> {
+			if (productRepository.findAll().isEmpty()) {
+				Product product = new Product(
+						null,
+						"Test product",
+						"Demo product",
+						new BigDecimal("1000.00"),
+						10,
+						"TEST",
+						"ACTIVE",
+						LocalDateTime.now()
+				);
+				productRepository.save(product);
+			}
+		};
+	}
 }
