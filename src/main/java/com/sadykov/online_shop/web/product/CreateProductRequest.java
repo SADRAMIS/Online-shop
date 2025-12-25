8c57595337c0bd86b5d8819ad4b8caeacdb93d23
Sadykov Ramis <ramis.sadykov.99@mail.ru>
2025-12-25 14:05:46 +0300

    CreateProductRequest

---
+++ src/main/java/com/sadykov/online_shop/web/product/CreateProductRequest.java
@@ -0,0 +1,52 @@
+package com.sadykov.online_shop.web.product;
+
+import java.math.BigDecimal;
+
+public class CreateProductRequest {
+
+    private String name;
+    private String description;
+    private BigDecimal price;
+    private int quantity;
+    private String category;
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public BigDecimal getPrice() {
+        return price;
+    }
+
+    public void setPrice(BigDecimal price) {
+        this.price = price;
+    }
+
+    public int getQuantity() {
+        return quantity;
+    }
+
+    public void setQuantity(int quantity) {
+        this.quantity = quantity;
+    }
+
+    public String getCategory() {
+        return category;
+    }
+
+    public void setCategory(String category) {
+        this.category = category;
+    }
+}
